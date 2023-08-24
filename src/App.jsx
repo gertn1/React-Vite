1fe20c84960172278a2bbdbf92964340a1d1100a
Germano Ribeiro <germanno.verde@gmail.com>
2023-08-23 21:49:19 -0300

    detalhes de posicionamento imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './app.css'
+import Foto1 from './assets/Foto1.jpg'
 import Foto2 from './assets/Foto2.jpg'
+import Foto3 from './assets/Foto3.jpg'
+import Foto4 from './assets/Foto4.jpg'
 
 
 
@@ -11,10 +14,28 @@ function App() {
     <h1 className='titulo'>Itens mais vendidos</h1>
     
    <div className='conteiner'>
-    <img className = "tenis" src={Foto2} />
-    <p className='descricao'> Tenis Básico <span>$35</span></p>
-   
+    <img className = "imagens" src={Foto1} />
+    <p className='descricao'> Tênis Nike Revolution 6<span>$35</span></p>
+    <p>branco</p>
    </div>
+
+   <div className='conteiner'>
+    <img className = "imagens" src={Foto2} />
+    <p className='descricao'> Tênis Nike Air Max SYSTM <span>$35</span></p>
+    <p>vermelho</p>
+   </div>
+   <div className='conteiner'>
+    <img className = "imagens" src={Foto3} />
+    <p className='descricao'> Terno Slim Kit Completo De Luxo<span>$35</span></p>
+    <p>branco</p>
+   </div>
+
+   <div className='conteiner'>
+    <img className = "imagens" src={Foto4} />
+    <p className='descricao'> Camisas Completo De Luxo<span>$35</span></p>
+    <p>varias cores</p>
+   </div>
+   
     </>
   )
 }
